b08920a3f3a9229affa92ab4dbba371ee4ebd35d
Maciej Jankowski <m.jankowski0@gmail.com>
2019-11-17 21:17:54 +0100

    added seller & prices list

--- src/item.py
+++ src/item.py
@@ -1,3 +1,5 @@
+from typing import List, Dict
+
 from src.item_query import ItemQuery
 from .constants import Ceneo
 
@@ -5,9 +7,10 @@ from .constants import Ceneo
 class Item:
     """ Class reflecting a specific item"""
 
-    prod_id = ""
+    prod_id: str = ""
     # TODO: consider leaving item_query as "parent" to item
-    parent_item_query = ""
+    parent_item_query: ItemQuery = None
+    prices: List[Dict] = []
 
     def __init__(self, prod_id: str, parent_item_query: ItemQuery = None):
         self.prod_id = prod_id
@@ -16,3 +19,7 @@ class Item:
     def create_url(self) -> str:
         """ Create URL for and item with specific ID sorted by lowest price (delivery included) """
         return f"{Ceneo.URI}{self.prod_id};0284-0.htm"
+
+    def add_seller(self, name: str, price: float):
+        """ Method to append seller with their price to prices """
+        self.prices.append({"name": name, "price": price})
