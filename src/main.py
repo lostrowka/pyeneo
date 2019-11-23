c777682af0a905f54e32f453c4f25b35e6627884
Lukasz Ostrowka <Lukasz.Ostrowka@motorolasolutions.com>
2019-11-23 19:38:36 +0100

    Add SearchResultsProcessor

--- src/main.py
+++ src/main.py
@@ -2,9 +2,14 @@ from src.CeneoAPIHandler import CeneoAPIHandler
 from src.models.item_query import ItemQuery
 from datetime import datetime
 
+from src.processors.search_results_processor import SearchResultsProcessor
+
 item_query = ItemQuery("pralka wsad 7 kg", 1, 1000, 1600, datetime.now())
 
 print(item_query.create_url())
 
 api_handler = CeneoAPIHandler()
 html = api_handler.send_search_request(item_query).text
+
+search_processor = SearchResultsProcessor(html, item_query)
+search_processor.get_first_item_with_multiple_sellers()
---
+++ src/processors/search_results_processor.py
@@ -0,0 +1,32 @@
+import logging
+from typing import List
+
+from bs4 import BeautifulSoup, element
+
+from src.models.item import Item
+from src.models.item_query import ItemQuery
+from src.processors import DataProcessorException
+
+
+class SearchResultsProcessor:
+    html_soup = ""
+
+    def __init__(self, html_doc: str, query: ItemQuery):
+        self.html_soup = BeautifulSoup(html_doc, 'html.parser')
+        self.log = logging.getLogger(SearchResultsProcessor.__name__)
+        self.query = query
+
+    def get_search_results_dom(self) -> element.Tag:
+        return self.html_soup.find("div", class_="js_search-results")
+
+    def get_item_results_list(self) -> List[element.Tag]:
+        return self.get_search_results_dom().find_all("div", class_="js_category-list-item")
+
+    def get_first_item_with_multiple_sellers(self) -> Item:
+        """ Get first item with more than one seller """
+        for item_dom in self.get_item_results_list():
+            go_to_dom: element.Tag = item_dom.select_one("a.go-to-product.btn")
+            if go_to_dom:
+                self.log.info(f"Found item {go_to_dom['title']}")
+                return Item(go_to_dom['href'], self.query)
+        raise DataProcessorException(f"No item with more than 1 seller found in search results.")
