c788e357336cdaf287f733a22e3d0183a11de976
Lukasz Ostrowka <lostrowka@gmail.com>
2019-11-24 11:24:02 +0100

    Add ProductOffersProcessor and Offer classes

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from src.CeneoAPIHandler import CeneoAPIHandler
 from src.models.item_query import ItemQuery
 from datetime import datetime
 
+from src.processors.product_offers_processor import ProductOffersProcessor
 from src.processors.search_results_processor import SearchResultsProcessor
 
 item_query = ItemQuery("pralka wsad 7 kg", 1, 1000, 1600, datetime.now())
@@ -9,7 +10,14 @@ item_query = ItemQuery("pralka wsad 7 kg", 1, 1000, 1600, datetime.now())
 print(item_query.create_url())
 
 api_handler = CeneoAPIHandler()
-html = api_handler.send_search_request(item_query).text
+item_query_html = api_handler.send_search_request(item_query).text
 
-search_processor = SearchResultsProcessor(html, item_query)
-search_processor.get_first_item_with_multiple_sellers()
+search_processor = SearchResultsProcessor(item_query_html, item_query)
+item = search_processor.get_first_item_with_multiple_sellers()
+
+print(item.create_url())
+
+item_html = api_handler.send_product_request(item).text
+product_processor = ProductOffersProcessor(item_html, item)
+
+product_processor.get_offers_list()
---
+++ src/models/offer.py
@@ -0,0 +1,10 @@
+class Offer:
+    """ Object containing single offer for a specific product """
+
+    def __init__(self, name: str, price: float, url: str, rating: float, opinions: int):
+        """ Create new Offer """
+        self.name = name
+        self.price = price
+        self.url = url
+        self.rating = rating
+        self.opinions = opinions
