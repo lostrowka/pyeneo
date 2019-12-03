05a5bfb89f5032ffaede58846459df601d21241f
Maciej Jankowski <m.jankowski0@gmail.com>
2019-12-03 23:32:58 +0100

    formatting fixes and cleanup

--- server/website/ceneo_api_handler.py
+++ server/website/ceneo_api_handler.py
@@ -1,6 +1,6 @@
-import requests
 import logging
 
+from requests import Response, request
 from server.website.models.item import Item
 from server.website.models.item_query import ItemQuery
 
@@ -12,14 +12,14 @@ class CeneoAPIHandler:
         self.log_GET = "sending GET to {URL}"
         self.log = logging.getLogger(CeneoAPIHandler.__name__)
 
-    def send_search_request(self, item_query: ItemQuery) -> requests.Response:
+    def send_search_request(self, item_query: ItemQuery) -> Response:
         """ Serve HTTP GET request regarding desired item """
         url = item_query.create_url()
         self.log.info(self.log_GET.format(URL=url))
-        return requests.request('GET', url)
+        return request('GET', url)
 
-    def send_product_request(self, item: Item) -> requests.Response:
+    def send_product_request(self, item: Item) -> Response:
         """ Serve HTTP GET request regarding specific item to obtain sorted list of prices """
         url = item.create_url()
         self.log.info(self.log_GET.format(URL=url))
-        return requests.request('GET', url)
+        return request('GET', url)
