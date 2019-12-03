05418bfd8117ca7e18326cdad6fe03988ae92d3e
Lukasz Ostrowka <lostrowka@gmail.com>
2019-12-03 23:58:14 +0100

    Clean unused code

--- server/website/urls.py
+++ server/website/urls.py
@@ -4,11 +4,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 
 urlpatterns = [
-    path('make_a_request/', views.request_page, name='request_page'),
-    # path('output/<str:task_id>/', views.output_page, name='output_page'),
-    # path('loading/<str:task_id>/', views.loading_page, name='loading_page'),
-    # path('check_output/<str:task_id>/', views.check_output, name='check_output'),
-    path('', views.redirect_to_request, name='redirect_to_request_page'),
+    path('', views.request_page, name='request_page'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
