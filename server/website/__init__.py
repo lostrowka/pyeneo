0bffced973f6e8828da84e41dfcdb07a82471ed1
Maciej Jankowski <m.jankowski0@gmail.com>
2019-12-04 23:36:31 +0100

    log path fix

--- server/website/__init__.py
+++ server/website/__init__.py
@@ -2,7 +2,7 @@ import logging
 
 # Create global log file on application startup
 # TODO: color logs, set debug logging level as CLI arg --debug
-logging.basicConfig(filename='../logs/pyeneo.log',
+logging.basicConfig(filename='logs/pyeneo.log',
                     filemode='w',
                     format='%(asctime)s,%(msecs)3d | %(name)-22s | %(levelname)-7s %(message)s',
                     datefmt='%H:%M:%S',
