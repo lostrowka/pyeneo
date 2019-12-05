37061ac1f5765c658fa8035470b2255cca17e2ac
koall4 <31703026+koall4@users.noreply.github.com>
2019-12-05 17:56:38 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-apt-get update && apt-get upgrade -y
+apt-get update && apt-get upgrade -y  
 apt-get install python3.7 -y
 apt-get install git-all -y
-apt-get install pipenv -y
 mkdir io
 cd io
 git clone https://github.com/lostrowka/pyeneo.git
-python3.7 -m pip install venv
+apt-get install python3-venv
 python3.7 -m venv my_env
 source my_env/bin/activate
 cd pyeneo
