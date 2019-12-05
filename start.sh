107e372b7579087aff74f8a361165580b58d6746
koall4 <31703026+koall4@users.noreply.github.com>
2019-12-05 13:39:20 +0100

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo su
+apt-get update && apt-get upgrade -y
+apt-get install python3.7 -y
+apt-get install git-all -y
+apt-get install pipenv -y
+mkdir io
+cd io
+git clone https://github.com/lostrowka/pyeneo.git
+python3.7 -m pip install venv
+python3.7 -m venv my_env
+source my_env/bin/activate
+python -m pip -r
+cd pyeneo
+export PYTHONPATH=$(pwd)
+python server/website manage.py runserver
